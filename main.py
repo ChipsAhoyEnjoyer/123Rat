1a298a4f58512dd44cc43add7f3673b188ffa054
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2025-01-06 01:50:47 +0000

    Added draw_page_xy func to main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+from reportlab.pdfgen import canvas
+
+
+FILENAME = "123RatWorksheet.pdf"
+
+
+def draw_page_xy(worksheet):
+    for i in range(0, 11):
+        worksheet.drawString(i * 100, 0, f"x={i*100}")
+        worksheet.drawString(0, i * 100, f"y={i*100}")
+
+
+def main():
+    worksheet = canvas.Canvas(FILENAME)
+    draw_page_xy(worksheet)
+    worksheet.save()
+
+
+if __name__ == "__main__":
+    main()

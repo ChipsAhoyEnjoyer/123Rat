f612dfec7eb48b69600eb41a0e9a5caa27ef5ae9
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2025-01-09 03:20:59 +0000

    added test.py file

---
+++ test.py
@@ -0,0 +1,12 @@
+from worksheet import Worksheet
+
+
+class TestWorksheet:
+    def__init__(self, filename):
+        self.example = Worksheet(filenaem)
+
+    def test_write():
+        pass
+
+    def test_next_page():
+        pass
